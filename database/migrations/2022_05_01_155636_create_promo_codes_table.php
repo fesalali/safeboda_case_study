a82e99542c19c46abb820cf6e90b69ddf2a3bad7
fesalali <fesalali05@gmail.com>
2022-05-03 19:16:15 +0300

    create migrations

---
+++ database/migrations/2022_05_01_155636_create_promo_codes_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePromoCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('promo_codes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('event_id');
+            
+            $table->foreign('event_id', 'events_promocodes')
+            ->references('id')->on('events')
+            ->onDelete('cascade')
+            ->onUpdate('restrict');
+
+            $table->string("code",10)->unique();
+            $table->tinyInteger('is_active')->length(1)->nullable()->default(1);
+            $table->integer("available_count");
+            $table->decimal("destination");
+            $table->date("expiration_date")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('promo_codes');
+    }
+}
