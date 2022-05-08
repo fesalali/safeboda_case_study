b02e27dbc5db8579b882a6681c460f91e85a52c2
fesalali <fesalali05@gmail.com>
2022-05-08 16:22:11 +0300

    add test for relation belongsto

--- tests/Feature/PromoCodeTest.php
+++ tests/Feature/PromoCodeTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Models\Event;
 use App\Models\PromoCode;
+use Carbon\Factory;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
@@ -34,26 +35,26 @@ class PromoCodeTest extends TestCase
         ]);
     }
 
-    public function test_user_can_see_list_promo_code()
+    public function test_user_can_see_list_promocode()
     {
         $response = $this->get('api/promo_codes');
         $response->assertStatus(200);
     }
 
-    public function test_user_can_see_list_active_promo_code()
+    public function test_user_can_see_list_active_promocode()
     {
         $response = $this->get('api/promo_codes/active');
         $response->assertStatus(200);
     }
 
 
-    public function test_user_can_see_list_promo_code_in_active()
+    public function test_user_can_see_list_in_active_promocode()
     {
         $response = $this->get('api/promo_codes/inActive');
         $response->assertStatus(200);
     }
 
-    public function test_user_can_store_promo_codes()
+    public function test_user_can_store_promocodes()
     {
 
         $response = $this->call('POST', 'api/promo_codes', [
@@ -65,7 +66,7 @@ class PromoCodeTest extends TestCase
         $this->assertEquals(201, $response->getStatusCode());
     }
 
-    public function test_user_can_active_promo_codes()
+    public function test_user_can_active_promocodes()
     {
 
         $response = $this->call('PUT','api/promo_codes/'.$this->promoCode->id.'/active');
@@ -74,11 +75,23 @@ class PromoCodeTest extends TestCase
     }
 
 
-    public function test_user_can_in_active_promo_codes()
+    public function test_user_can_in_active_promocodes()
     {
 
         $response = $this->call('PUT','api/promo_codes/'.$this->promoCode->id.'/inActive');
 
         $this->assertEquals(200, $response->getStatusCode());
     }
+
+
+    public function test_promocode_can_belongs_to_event()
+    {
+        $this->assertDatabaseHas("promo_codes",[
+            'id'=>$this->promoCode->id,
+            'event_id'=>$this->event->id
+        ]);
+    }
+
+
+    
 }
